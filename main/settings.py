ee03f3df77245d7d39c45cf37a5c1ec337a13368
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-23 19:56:50 +0530

    removed bootstrap datepicker plus module

--- main/settings.py
+++ main/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_bootstrap5',
-    'bootstrap_datepicker_plus',
+    # 'bootstrap_datepicker_plus',
     'core',
     'bookings',
     'clients',
