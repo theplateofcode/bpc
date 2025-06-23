e39055dc74a6352e5e4351bee39c7741914ebdbe
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-23 19:40:32 +0530

    adding requirements.txt file

--- main/settings.py
+++ main/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-t^8)i6mbc!hb_6%^(^bkaw^9cw186#7)6pmh_gy@8^cj#y_2!p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.35.129', 'localhost', ]
 
 DATE_INPUT_FORMATS = ['%d/%m/%y']  # Add as needed
 # Application definition
@@ -92,15 +92,15 @@ WSGI_APPLICATION = 'main.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'bpc',         # <-- your database name
-        'USER': 'root',         # <-- your MySQL user (or 'root' if you prefer)
-        'PASSWORD': 'Gaynegus@420',    # <-- your MySQL password
+        'ENGINE': 'django.db.backends.mypostgresql',
+        'NAME': 'mydb',         # <-- your database name
+        'USER': 'myuser',         # <-- your MySQL user (or 'root' if you prefer)
+        'PASSWORD': 'mypassword',    # <-- your MySQL password
         'HOST': 'localhost',           # or '127.0.0.1'
         'PORT': '3306',                # default MySQL port
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        # 'OPTIONS': {
+        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        # },
     }
 }
 
