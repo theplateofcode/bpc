8e5f809a5261458156bd4ae3e72b82d7163d65b7
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-23 19:48:14 +0530

    added the static root path format

--- main/settings.py
+++ main/settings.py
@@ -144,6 +144,8 @@ MEDIA_ROOT = os.path.join('bpc.media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
