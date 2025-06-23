b2a161a2e3ae3036183569d3cbfb89933d77564c
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-23 19:54:14 +0530

    changed the bootstrap to django-bootstrap

--- main/settings.py
+++ main/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap5',
+    'django-bootstrap5',
     'bootstrap_datepicker_plus',
     'core',
     'bookings',
