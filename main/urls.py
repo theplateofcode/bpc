f26a3d2aaa985613ecd111a438dd02cb171c48ae
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-24 16:39:29 +0530

    added collect static

--- main/urls.py
+++ main/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
