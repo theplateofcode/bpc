426cb8390e28dc4c524042525e11ac98a6ba5c13
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-24 14:22:40 +0530

    added django cors and related settings

--- main/settings.py
+++ main/settings.py
@@ -53,10 +53,13 @@ INSTALLED_APPS = [
     'accounts',
     'widget_tweaks',
     'xhtml2pdf',
+    'corsheaders'
 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # must be first
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -106,7 +109,10 @@ DATABASES = {
         },
     }
 }
-
+CORS_ALLOWED_ORIGINS = [
+    "http://192.168.35.129:8000",
+    "http://localhost:8000",
+]
 
 
 # Password validation
