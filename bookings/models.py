02d8231a6559fcf4439aa4751a5751372062587f
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-06-27 15:24:11 +0530

    default status

--- bookings/models.py
+++ bookings/models.py
@@ -69,7 +69,7 @@ class Booking(models.Model):
     status = models.ForeignKey(
         Status,
         on_delete=models.PROTECT,
-        default=1
+        default=4
     )
     accounts_done = models.BooleanField(
         default=False, verbose_name="Accounts Processed")
