e5853d6beff903d33cd368d5bcb827404e8e9892
roe jogan <164974008+ynwhate@users.noreply.github.com>
2025-12-16 22:09:17 +0530

    role accounts => accountant

--- payments/views.py
+++ payments/views.py
@@ -20,7 +20,7 @@ from .models import PaymentReceived, Mode
 
 
 User = get_user_model()
-ACCOUNTS_GROUP_NAME = "Accounts"  # change if your group name differs
+ACCOUNTS_GROUP_NAME = "ACCOUNTANT"  # change if your group name differs
 
 
 # =========================
@@ -304,7 +304,7 @@ def payment_approvals(request):
     items = PaymentReceived.objects.filter(
         sent_for_approval=True, approved=False
     ).order_by("created_at")
-    return render(request, "payments/accountant_dashboard.html", {"items": items})
+    return render(request, "payments/approvals.html", {"items": items})
 
 
 @login_required
